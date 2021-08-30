f6ef4cd4ba9546792f123614ee27db12bc1a513b
LUCASRENAA <lucasrenaa1996@gmail.com>
2021-08-29 22:12:35 -0300

    testando uns selects

--- academia.py
+++ academia.py
@@ -1,6 +1,16 @@
 import mysql.connector
 import getpass
+from validate_docbr import CPF
+from criartabelas import criarTabelas
+cpf = CPF()
 
+def pegarId(linha):
+    for x in linha:
+                print(x)
+                for y in x:
+                    iddogrupo = y 
+    return iddogrupo
+    
 def conectarnobanco(texto,host,user,password,opcao):
     con = mysql.connector.connect(host=host,database='academia',user=user,password=password)
     if con.is_connected():
@@ -11,7 +21,7 @@ def conectarnobanco(texto,host,user,password,opcao):
         cursor.execute(texto)
         #linha = cursor.fetchone()
         linha=  cursor.fetchall()
-        print("Conectado ao banco de dados ",linha)
+        #print("Conectado ao banco de dados ",linha)
     if opcao == 1:
         con.commit()
     if con.is_connected():
@@ -22,77 +32,219 @@ def conectarnobanco(texto,host,user,password,opcao):
     return linha
 
 print("-" * 30 + str("SISTEMA DE ACADEMIA DO SEU ZÉ") + "-" * 30) 
+
 host = input("Digite o host que você quer acessar: ")
 user = input("Digite agora o usuário do banco: ")
 password = getpass.getpass("Digite sua senha: ")
 
-academia  = input("Digite o nome da sua academia: ")
+menu = ""
+while menu != "5":
+    menu = input("""Digite a opção do menu\n
+1- Criação do banco(Criar banco e tabelas)\n
+2- Administrativo (Criar Academias,Grupos e ver esses grupos)\n
+3- Atendente(Organizar a fila de espera e verificar se o aluno é da academia\n
+4- Personal trainer(Criar listas de exercicios)\n
+5- Sair\n""")
 
-#verificar se a academia existe
-
-
-if academia == "1":
-
-    print("1-Criar Grupo")
-    print("2- Criar Academia")
-    print("3-Ver Grupos")
-    print("4- Ver Academias")
-    opcao = int(input())
-
-    if opcao == 1:
-
-        texto = input("Digite o nome do grupo: ")
-        texto = "INSERT INTO GRUPO (NomeGrupo) Values ('{}')".format(texto)
-       
-    
-
-        print(texto)
-        conectarnobanco(texto,host,user,password,1)
+    if menu == "5":
+        print("OPS SAINDO")
+        print("-" * 30 + str("SISTEMA DE ACADEMIA DO SEU ZÉ") + "-" * 30) 
         
-     
-    if opcao == 2:
-        nome = input("Digite o nome da academia: ")
-        endereco = input("Digite o endereco da academia: ")
-        dono = input("Digite o nome do dono da academia: ")
-        grupo = input("Digite o nome do grupo da academia: ")
-        texto = "SELECT ID_GROUP FROM academia.GRUPO WHERE  NomeGrupo = '" + str(grupo) + "'"
-        linha = conectarnobanco(texto,host,user,password,0)
-        for x in linha:
-            print(x)
-            for y in x:
-                iddogrupo = y 
+    else:
+        if menu == "1":
+            criarTabelas(host,user,password)
 
-        texto = "INSERT INTO ACADEMIA (NomeAcademia,Endereco,Dono,ID_GROUP) Values ('{}','{}','{}',{})".format(nome,endereco,dono,iddogrupo)
-       
-    
 
-        print(texto)
-        conectarnobanco(texto,host,user,password,1)
-    if opcao == 3:
-        texto = "SELECT * FROM academia.GRUPO"
-        linha = conectarnobanco(texto,host,user,password,0)
+        #verificar se a academia existe
 
-        
+
+        if menu == "2":
+
+            print("1-Criar Grupo")
+            print("2- Criar Academia")
+            print("3-Ver Grupos")
+            print("4- Ver Academias")
+            opcao = int(input())
+
+            if opcao == 1:
+
+                texto = input("Digite o nome do grupo: ")
+                texto = "INSERT INTO GRUPO (NomeGrupo) Values ('{}')".format(texto)
+            
             
 
-    if opcao == 4:
-        texto = "SELECT * FROM academia.ACADEMIA"
-        linha = conectarnobanco(texto,host,user,password,0)
+                print(texto)
+                conectarnobanco(texto,host,user,password,1)
+                
+            
+            if opcao == 2:
+                nome = input("Digite o nome da academia: ")
+                endereco = input("Digite o endereco da academia: ")
+                dono = input("Digite o nome do dono da academia: ")
+                grupo = input("Digite o nome do grupo da academia: ")
+                
+                lotacao = int(input("Digite o nome a lotacao da academia: "))
+                
 
-        for x in linha:
-            print(x)
+                texto = "SELECT ID_GROUP FROM academia.GRUPO WHERE  NomeGrupo = '" + str(grupo) + "'"
+                linha = conectarnobanco(texto,host,user,password,0)
+                print("Academia está aqui: " + str(linha))
+                iddogrupo = pegarId(linha)
+
+                texto = "INSERT INTO ACADEMIA (NomeAcademia,Endereco,Dono,ID_GROUP,quantidadeAlunos,lotacao) Values ('{}','{}','{}',{},0,{})".format(nome,endereco,dono,iddogrupo,lotacao)
+            
+            
+
+                print(texto)
+                conectarnobanco(texto,host,user,password,1)
 
 
 
+
+
+                texto = "SELECT id_ACADEMIA FROM academia.ACADEMIA WHERE  NomeAcademia = '" + str(nome) + "'"
+                linha = conectarnobanco(texto,host,user,password,0)
+                print(linha)
+                idAcademia = pegarId(linha)
+
+
+            
+
+
+
+
+            if opcao == 3:
+                texto = "SELECT * FROM academia.GRUPO"
+                linha = conectarnobanco(texto,host,user,password,0)
+                print(linha)
+
+                
+                    
+
+            if opcao == 4:
+                texto = "SELECT * FROM academia.ACADEMIA"
+                linha = conectarnobanco(texto,host,user,password,0)
+
+                for x in linha:
+                    print(x)
+
+
+
+
+            
+
+
+        if menu == "3":
+            academia  = input("Digite o nome da sua academia: ")
+
+            vetoracademia = []
+            print("Verificando a existencia dessa academia")
+            texto = "SELECT * FROM academia.ACADEMIA"
+            linha = conectarnobanco(texto,host,user,password,0)
+            for i in linha:
+                    print(i)
+                    if i[2] == academia:
+                        vetoracademia = i
+            if vetoracademia != []:
+                print("Academia existe :D")
+                print(i[2])
+            else:
+                print("Academia não existe")
+                
+            
+
+
+
+
+            print("O que você deseja fazer?")
+            print("1- Cadastrar Pessoa ou Personal")
+            print("2- Ver Pessoas")
+     
 
     
+            escolher = input("O que você deseja fazer?")
+            if escolher == "1":
+                cpf_aluno = input("Digite o cpf da pessoa: ")
+                while cpf.validate(cpf_aluno) == False:
+                    print("Opa, digita o CPF certinho ai amigão :D")
+                    cpf_aluno = input("Digite o cpf do pessoa: ")
+
+                nome_aluno = input("Digite o nome do pessoa: ")
+                data_aluno = input("Digite a data de nascimento do pessoa: ")
+                celular_aluno = input("Digite o  numero de celular do pessoa: ")
+                tipo_sanguineo_aluno = input("Digite o tipo sanguineo do pessoa: ")
+
+                texto = "INSERT INTO Pessoa (CPF,Nome_Pessoa,Data_Nascimento,Numero_Celular,Tipo_Sanguineo) Values ('{}','{}','{}','{}','{}')".format(cpf_aluno,nome_aluno,data_aluno,celular_aluno,tipo_sanguineo_aluno)
+        
+                print(texto)
+                conectarnobanco(texto,host,user,password,1)
 
 
-else:
-    print("O que você deseja fazer?")
-    print("1- Cadastrar Aluno")
-    print("2- Cadastrar Personal")
-    print("O que você deseja fazer?")
+
+                texto = "SELECT ID_GROUP FROM academia.ACADEMIA WHERE  NomeAcademia = '" + str(academia) + "'"
+                linha = conectarnobanco(texto,host,user,password,0)
+                iddogrupo = pegarId(linha)
 
 
-print("-" * 30 + str("SISTEMA DE ACADEMIA DO SEU ZÉ") + "-" * 30) 
+                texto = "SELECT id_ACADEMIA FROM academia.ACADEMIA WHERE  NomeAcademia = '" + str(academia) + "'"
+                linha = conectarnobanco(texto,host,user,password,0)
+                iddaacademia = pegarId(linha)
+
+
+                escolha = input("Aluno ou personal?")
+                if escolha == "1":
+                    texto = "INSERT INTO CADASTRO_ALUNO (Comprovante,ID_GROUP,CPF) Values ('{}',{},'{}')".format("FEITO",iddogrupo,cpf_aluno)
+                    print(texto)
+                    conectarnobanco(texto,host,user,password,1)
+                if escolha == "2":
+                    formacao = input("Digite a formação do Personal: ")
+                    estadocivil = input("Digite o estado civil 'S', 'C', 'D', 'V'")
+                    texto = "INSERT INTO CADASTRO_PERSONAL (CPF,id_ACADEMIA,Formacao,estadocivil,Comprovante) Values ('{}',{},'{}','{}','Feito')".format(cpf_aluno,iddaacademia,formacao,estadocivil)
+                    print(texto)
+                    conectarnobanco(texto,host,user,password,1)
+
+
+
+            if escolher == "2":
+                texto = "SELECT * FROM academia.Pessoa"
+                linha = conectarnobanco(texto,host,user,password,0)
+                print(linha)
+
+                texto = "SELECT * FROM academia.CADASTRO_ALUNO"
+                linha = conectarnobanco(texto,host,user,password,0)
+                print(linha)
+
+                texto = "SELECT * FROM academia.CADASTRO_PERSONAL"
+                linha = conectarnobanco(texto,host,user,password,0)
+                print(linha)
+
+                texto = """SELECT NomeAcademia,quantidadeAlunos
+FROM academia.CADASTRO_PERSONAL
+INNER JOIN academia.ACADEMIA 
+ON CADASTRO_PERSONAL.id_ACADEMIA = ACADEMIA.id_ACADEMIA
+"""
+                linha = conectarnobanco(texto,host,user,password,0)
+                print(linha)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+ quantidadeAlunos = int(input("Digite a quantidade de alunos máxima na academia"))
+            texto = "INSERT INTO FILA_ESPERA (Horario,id_ACADEMIA,Posicao,CPF) Values (now(),{},0,{},'N','{}')".format(idAcademia,quantidadeAlunos,cpf_aluno)
+        
+        
+
+            print(texto)
+            conectarnobanco(texto,host,user,password,1)
+"""
--- criartabelas.py
+++ criartabelas.py
@@ -2,6 +2,7 @@ import mysql.connector
 import getpass
 
 
+
 def conectarnobanco(texto,host,user,password):
     con = mysql.connector.connect(host=host,database='academia',user=user,password=password)
     if con.is_connected():
@@ -16,230 +17,183 @@ def conectarnobanco(texto,host,user,password):
         cursor.close()
         con.close()
         print("Conexão ao MySQL foi encerrada")
-
-
-print("-" * 30 + "Criando banco de dados" + "-" * 30)
-host = input("Digite o host que você quer acessar: ")
-user = input("Digite agora o usuário do banco: ")
-password = getpass.getpass("Digite sua senha: ")
-
-
-
-print("Se conectando ao banco")
-try:
-  print("Verificando os dados")
-  cnx = mysql.connector.connect(host=host,user=user,password=password)
-
-  mycursor = cnx.cursor()
-  mycursor.execute("CREATE DATABASE academia")
-  print("Banco criado com sucesso :D")
-  
-except:
-  print("ERRO")
-
-
-textos = []
-print("-" * 30 + "Criar tabelas" + "-" *30)
-
-texto = """
-CREATE TABLE ALUNO (
- CPF VARCHAR(11) PRIMARY KEY ,
- Nome_Aluno VARCHAR(40),
- Data_Nascimento    DATE,
- Numero_Celular VARCHAR(14),
-  Tipo_Sanguineo VARCHAR(3)
- );
-"""
-textos.append(texto)
-
-
-
-texto = """
-CREATE TABLE GRUPO (
-    ID_GROUP int NOT NULL AUTO_INCREMENT,
-  NomeGrupo varchar(255),
-    PRIMARY KEY (ID_GROUP)
- );
-"""
-textos.append(texto)
+def criarTabelas(host,user,password):
+    print("-" * 30 + "Criando banco de dados" + "-" * 30)
 
 
 
 
-texto = """
-CREATE TABLE CADASTRO_ALUNO (
-    ID_CADASTRO_ALUNO int NOT NULL AUTO_INCREMENT,
-  Comprovante varchar(255),
-      ID_GROUP int,
-      CPF VARCHAR(11),
-
-    PRIMARY KEY (ID_CADASTRO_ALUNO),
-    FOREIGN KEY (ID_GROUP) REFERENCES GRUPO(ID_GROUP),
-    FOREIGN KEY (CPF) REFERENCES ALUNO(CPF)
-
- );
-"""
-textos.append(texto)
-
-
-texto = """
-CREATE TABLE CADASTRO_ALUNO (
-    ID_CADASTRO_ALUNO int NOT NULL AUTO_INCREMENT,
-  Comprovante varchar(255),
-      ID_GROUP int,
-      CPF VARCHAR(11),
-
-    PRIMARY KEY (ID_CADASTRO_ALUNO),
-    FOREIGN KEY (ID_GROUP) REFERENCES GRUPO(ID_GROUP),
-    FOREIGN KEY (CPF) REFERENCES ALUNO(CPF)
-
- );
-"""
-textos.append(texto)
-
-
-
-texto = """
-CREATE TABLE ACADEMIA (
-    id_ACADEMIA int NOT NULL AUTO_INCREMENT,
-      ID_GROUP int,
-      
-      NomeAcademia VARCHAR(50),
-
-      Endereco VARCHAR(50),
-      Dono VARCHAR(50),
-
-    PRIMARY KEY (id_ACADEMIA),
-    FOREIGN KEY (ID_GROUP) REFERENCES GRUPO(ID_GROUP)
-
- );
-"""
-textos.append(texto)
-
-
-texto = """
-CREATE TABLE FILA_ESPERA (
-    ID_FILA_ESPERA int NOT NULL AUTO_INCREMENT,
-  Horario DATETIME,
-      id_ACADEMIA int,
-        Posicao int,
-        quantidadeAlunos int,
-        lotacao int,
-      CPF VARCHAR(11),
-    PRIMARY KEY (ID_FILA_ESPERA),
-    FOREIGN KEY (id_ACADEMIA) REFERENCES ACADEMIA(id_ACADEMIA),
-    FOREIGN KEY (CPF) REFERENCES ALUNO(CPF)
-
- );
-"""
-textos.append(texto)
-
-
-
-
-texto = """
-CREATE TABLE FILA_ESPERA (
-    ID_FILA_ESPERA int NOT NULL AUTO_INCREMENT,
-  Horario DATETIME,
-      id_ACADEMIA int,
-        Posicao int,
-        quantidadeAlunos int,
-        lotacao int,
-      CPF VARCHAR(11),
-    PRIMARY KEY (ID_FILA_ESPERA),
-    FOREIGN KEY (id_ACADEMIA) REFERENCES ACADEMIA(id_ACADEMIA),
-    FOREIGN KEY (CPF) REFERENCES ALUNO(CPF)
-
- );
-"""
-textos.append(texto)
-
-
-texto = """
-CREATE TABLE PERSONAL (
-    ID_PERSONAL int NOT NULL AUTO_INCREMENT,
-    CPF VARCHAR(11),
-  DataNascimento DATE,
-   Numero_Celular VARCHAR(14),
- Formacao VARCHAR(50),
-    estadocivil char(1) CHECK (estadocivil IN ('S', 'C', 'D', 'V') ),
-  Tipo_Sanguineo VARCHAR(3),
-
-      id_ACADEMIA int,
-    PRIMARY KEY (ID_PERSONAL),
-    FOREIGN KEY (id_ACADEMIA) REFERENCES ACADEMIA(id_ACADEMIA)
-
- );
-"""
-textos.append(texto)
-
-
-texto = """
-CREATE TABLE CADASTRO_PERSONAL (
-    ID_CADASTRO_PERSONAL int NOT NULL AUTO_INCREMENT,
-  Comprovante varchar(255),
-
-      id_ACADEMIA int,
-      ID_PERSONAL int,
-    PRIMARY KEY (ID_CADASTRO_PERSONAL),
-      FOREIGN KEY (ID_PERSONAL) REFERENCES PERSONAL(ID_PERSONAL),
-    FOREIGN KEY (id_ACADEMIA) REFERENCES ACADEMIA(id_ACADEMIA)
-
- );
-"""
-textos.append(texto)
-
-
-
-texto = """
-CREATE TABLE LISTA_EXERCICIOS (
-    ID int NOT NULL AUTO_INCREMENT,
-  DescricaoExercicios varchar(255),
-   CPF VARCHAR(11),
-      ID_GROUP int,
-      ID_PERSONAL int,
-    PRIMARY KEY (ID),
-      FOREIGN KEY (ID_PERSONAL) REFERENCES PERSONAL(ID_PERSONAL),
-     FOREIGN KEY (ID_GROUP) REFERENCES GRUPO(ID_GROUP),
-    FOREIGN KEY (CPF) REFERENCES ALUNO(CPF)
-
- );
-"""
-textos.append(texto)
-
-
-texto = """
-CREATE TABLE EXERCICIOS (
-    ID_EXERCICIOS int NOT NULL AUTO_INCREMENT,
-  Nome varchar(50),
-    Serie char(1) CHECK (Serie IN ('1', '2', '3', '4') ),
-      Repeticao int,
-  Tipo varchar(50),
-        Intervalo int,
-        ID int,
-
-    PRIMARY KEY (ID_EXERCICIOS),
- 
-    FOREIGN KEY (ID) REFERENCES LISTA_EXERCICIOS(ID)
-
- );
-"""
-textos.append(texto)
-
-for texto in textos:
-    print(texto)
+    print("Se conectando ao banco")
     try:
-        conectarnobanco(texto,host,user,password)
-        print("Tabela criada com sucesso")
+      print("Verificando os dados")
+      cnx = mysql.connector.connect(host=host,user=user,password=password)
 
+      mycursor = cnx.cursor()
+      mycursor.execute("CREATE DATABASE academia")
+      print("Banco criado com sucesso :D")
+      
     except:
-        #print(texto)
-        print("Opa, algum erro aconteceu na criação da tabela")
-        pass
+      print("ERRO EM CRIAR DATABASE ACADEMIA\n verifique se existe um banco de dados chamado academia no seu mysql")
+
+
+    textos = []
+    print("-" * 30 + "Criar tabelas" + "-" *30)
+
+    texto = """
+    CREATE TABLE Pessoa (
+    CPF VARCHAR(11) PRIMARY KEY ,
+    Nome_Pessoa VARCHAR(40),
+    Data_Nascimento    DATE,
+    Numero_Celular VARCHAR(14),
+      Tipo_Sanguineo VARCHAR(3)
+    );
+    """
+    textos.append(texto)
 
 
 
-#conectarnobanco(texto,host,user,password)
+    texto = """
+    CREATE TABLE GRUPO (
+        ID_GROUP int NOT NULL AUTO_INCREMENT,
+      NomeGrupo varchar(255),
+        PRIMARY KEY (ID_GROUP)
+    );
+    """
+    textos.append(texto)
 
 
-print("\n\n\n\n " + "-" * 30 + "CRIADO PARA O SISTEMA DA ACADEMIA DO SEU ZÉ" + "-" * 30)
+
+
+    texto = """
+    CREATE TABLE CADASTRO_ALUNO (
+        ID_CADASTRO_ALUNO int NOT NULL AUTO_INCREMENT,
+      Comprovante varchar(255),
+          ID_GROUP int,
+          CPF VARCHAR(11),
+
+        PRIMARY KEY (ID_CADASTRO_ALUNO),
+        FOREIGN KEY (ID_GROUP) REFERENCES GRUPO(ID_GROUP),
+        FOREIGN KEY (CPF) REFERENCES Pessoa(CPF)
+
+    );
+    """
+    textos.append(texto)
+
+
+
+    texto = """
+    CREATE TABLE ACADEMIA (
+        id_ACADEMIA int NOT NULL AUTO_INCREMENT,
+          ID_GROUP int,
+          
+          NomeAcademia VARCHAR(50),
+
+            quantidadeAlunos int,
+            lotacao int,
+
+          Endereco VARCHAR(50),
+          Dono VARCHAR(50),
+
+        PRIMARY KEY (id_ACADEMIA),
+        FOREIGN KEY (ID_GROUP) REFERENCES GRUPO(ID_GROUP)
+
+    );
+    """
+    textos.append(texto)
+
+
+    texto = """
+    CREATE TABLE FILA_ESPERA (
+        ID_FILA_ESPERA int NOT NULL AUTO_INCREMENT,
+      Horario DATETIME,
+          id_ACADEMIA int,
+            
+        
+          
+          Posicao int,
+          CPF VARCHAR(11),
+              FOREIGN KEY (CPF) REFERENCES Pessoa(CPF),
+
+        PRIMARY KEY (ID_FILA_ESPERA),
+        FOREIGN KEY (id_ACADEMIA) REFERENCES ACADEMIA(id_ACADEMIA)
+
+    );
+    """
+    textos.append(texto)
+
+
+
+
+
+
+    texto = """
+    CREATE TABLE CADASTRO_PERSONAL (
+        ID_CADASTRO_PERSONAL int NOT NULL AUTO_INCREMENT,
+      Comprovante varchar(255),
+
+          id_ACADEMIA int,
+        CPF VARCHAR(11),
+
+        Formacao VARCHAR(50),
+        estadocivil char(1) CHECK (estadocivil IN ('S', 'C', 'D', 'V') ),
+        PRIMARY KEY (ID_CADASTRO_PERSONAL),
+            FOREIGN KEY (CPF) REFERENCES Pessoa(CPF),
+        FOREIGN KEY (id_ACADEMIA) REFERENCES ACADEMIA(id_ACADEMIA)
+
+    );
+    """
+    textos.append(texto)
+
+
+
+    texto = """
+    CREATE TABLE LISTA_EXERCICIOS (
+        ID int NOT NULL AUTO_INCREMENT,
+      DescricaoExercicios varchar(255),
+      CPF VARCHAR(11),
+          ID_GROUP int,
+        PRIMARY KEY (ID),
+        FOREIGN KEY (ID_GROUP) REFERENCES GRUPO(ID_GROUP),
+        FOREIGN KEY (CPF) REFERENCES Pessoa(CPF)
+
+    );
+    """
+    textos.append(texto)
+
+
+    texto = """
+    CREATE TABLE EXERCICIOS (
+        ID_EXERCICIOS int NOT NULL AUTO_INCREMENT,
+      Nome varchar(50),
+        Serie char(1) CHECK (Serie IN ('1', '2', '3', '4') ),
+          Repeticao int,
+      Tipo varchar(50),
+            Intervalo int,
+            ID int,
+
+        PRIMARY KEY (ID_EXERCICIOS),
+    
+        FOREIGN KEY (ID) REFERENCES LISTA_EXERCICIOS(ID)
+
+    );
+    """
+    textos.append(texto)
+
+    for texto in textos:
+        print(texto)
+        try:
+            conectarnobanco(texto,host,user,password)
+            print("Tabela criada com sucesso")
+
+        except:
+            #print(texto)
+            print("Opa, algum erro aconteceu na criação da tabela")
+            #conectarnobanco(texto,host,user,password)
+
+
+
+
+    #conectarnobanco(texto,host,user,password)
+
+
+    print("\n\n\n\n " + "-" * 30 + "CRIADO PARA O SISTEMA DA ACADEMIA DO SEU ZÉ" + "-" * 30)
