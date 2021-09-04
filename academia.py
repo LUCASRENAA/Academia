03bb4c0ed92936a6f8598243dbcee11b18ac4448
LUCASRENAA <lucasrenaa1996@gmail.com>
2021-09-04 14:02:44 -0300

    acrescentando dados falsos

--- academia.py
+++ academia.py
@@ -2,6 +2,8 @@ import mysql.connector
 import getpass
 from validate_docbr import CPF
 from criartabelas import criarTabelas
+from conexao import conectarnobanco
+
 cpf = CPF()
 
 
@@ -12,26 +14,7 @@ def pegarId(linha):
                 for y in x:
                     iddogrupo = y 
     return iddogrupo
-    
-def conectarnobanco(texto,host,user,password,opcao):
-    con = mysql.connector.connect(host=host,database='academia',user=user,password=password)
-    if con.is_connected():
-        db_info = con.get_server_info()
-        print("Conectado ao servidor MySQL versão ",db_info)
-        cursor = con.cursor()
-        
-        cursor.execute(texto)
-        #linha = cursor.fetchone()
-        linha=  cursor.fetchall()
-        #print("Conectado ao banco de dados ",linha)
-    if opcao == 1:
-        con.commit()
-    if con.is_connected():
-        cursor.close()
-        con.close()
-        
-        print("Conexão ao MySQL foi encerrada")
-    return linha
+
 
 print("-" * 30 + str("SISTEMA DE ACADEMIA DO SEU ZÉ") + "-" * 30) 
 
